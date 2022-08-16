a750bcf84fac54a490d4fb0e8fb247b61e9cf9fc
Paul Nicholas <liquidream@hotmail.co.uk>
2022-08-16 08:40:32 +0100

    Now with PICO-8 logo! (and conditional render)

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,22 +8,19 @@
       width="800"
     >
       <h1 class="text-h3">PICO-8 Web Tools</h1>
-      <span class="text-body-1">sdsdsdsfsdf</span>
+      <span class="text-body-1">This simple QR code generator was designed for use with PICO-8 - specifically generating codes for creations made with the online <a href="https://www.pico-8-edu.com" target="_blank">Education Edition of PICO-8.</a></span>
 
       <h2 class="text-h4 pt-5">QR-Code Generator</h2>
-      <span class="text-body-1">sdsdsdsfsdf</span>
+      <span class="text-body-1">Simply paste in the URL of your creation (after doing <strong>SAVE @URL</strong> in PICO-8) and click to generate QR code.</span>
       <v-form ref="form">
         <v-text-field
           v-model="inputUrl"
+          color="white"
           label="URL to PICO-8 Content"
           hint="e.g. 'https://www.pico-8-edu.com/?c=...''"
           required
         ></v-text-field>
-        <v-btn
-          color="success"
-          class="mr-4"
-          @click="generateCode"
-        >
+        <v-btn color="success" class="mr-4" @click="generateCode">
           Generate
         </v-btn>
       </v-form>
@@ -36,28 +33,64 @@
         </v-card-item>
 
         <v-card-text>This is content</v-card-text>
-      </v-card> --> 
+      </v-card> -->
       <div class="mt-5 mx-auto" width="200px">
-        <vue-qrcode v-if="qrUrl" :value="qrUrl" ></vue-qrcode>
+        <vue-qrcode v-if="qrUrl" :value="qrUrl" @ready="onReady"></vue-qrcode>
       </div>
     </v-sheet>
   </v-sheet>
 </template>
 
-<script setup lang="ts">  
-  console.log("hello script setup");
+<script setup lang="ts">
+console.log("hello script setup");
 
-  import { ref } from "vue";
-  import VueQrcode from '@chenfengyuan/vue-qrcode';
+import { ref } from "vue";
+import VueQrcode from "@chenfengyuan/vue-qrcode";
 
-  const inputUrl = ref("");
-  const qrUrl = ref("");
+const inputUrl = ref("");
+const qrUrl = ref("");
 
-  // https://www.pico-8-edu.com/?c=cHJpbnQgImhlbGxvIHdvcmxkIg==&g=w-w-w-w1HQHw-w2Xw-w3Xw-w2HQH
-  // https://www.pico-8-edu.com/?c=AHB4YQOnAslvcM4NR1zyCNEZ9yfnBssG3-u8wfmXv8GD5E8QPsHxdfMEzUYz9wjtS0RPED1BUjRPUgx02Ss8RLxUh0mXRO8QJHVf9ebo_tQeh4VhJBkw0KSlQkDwDk3cdXU2UUddH1ZRd5loQNg24WVF3YVdVhUv8RDV1kYQln35MEW1MjAzJStyXRBkc5NzV0ZDY0c2E_1bJP3GRLZVPEfU19HcVq5A0D1HNRbOLLaR4EBQBFVW1mvRRPYcC1ExsVadVr3OWwT95sCNR86sFwOj9-Zji8ViccCQkaLm1CZbKRbWxhbK8tbHqG_NXmpl4crX2FzKp-LlwWSjKAy2ZMA9a4x31QHdxPTa6ly8U681Fl8-YfHJBSoJlF2YZqWWQZW2U8oBmRPCJjdNOF64emZvOtsI9vZO2Fp4kvqCPImCvXKmbZ-kiG5DtCDRJwiM4ISVzeTi5JQiq5OoqKrkhqJJkimBgIvfpCkXhk4YXzth7IZo_EW6qd2tcndoaGtAASm64ICBPitP3pkpWkd0verJ2lYdTt1cpPuFDxaSG0YWViYyZZV_ZCFNh8Kl4aSr25V4Jd2YyfIV_YB2Jb0l7tI4uDipiglLmB1lFGbaEAPKLPP6IVmUTbjpBDeMDdjplpU_1yFotybDcGHFBQvJYp62ebsWpGVpTStEkS9Xi6E4Wu2SQT8tJGGattFKFJmqLKdm91YXhs7eaDcMc8UBVxZzK8XIxAlpFA_KkQiSDuxNjYTzW1IKQevEpJUnaY8o083JbKurTnBBvam5UldTWT6wVi9Eg9FisDubqgQIR9TxYFn3C3NKAhtVqRdUZyYoFU2a0jeRokkUZhN7kgwLqR7SRjUzY4u9uQd4i_NfYC56h_E3OP8ZmofIym4rf4ZckWJupbpgZGZuZ0uYonyBdxh4icdInuEtHmJpKa0nr0g2r9hLRlakTAA=&g=w-w-w-w1HQHw-w2Xw-w3Xw-w2HQH
+// https://www.pico-8-edu.com/?c=cHJpbnQgImhlbGxvIHdvcmxkIg==&g=w-w-w-w1HQHw-w2Xw-w3Xw-w2HQH
+// https://www.pico-8-edu.com/?c=AHB4YQOnAslvcM4NR1zyCNEZ9yfnBssG3-u8wfmXv8GD5E8QPsHxdfMEzUYz9wjtS0RPED1BUjRPUgx02Ss8RLxUh0mXRO8QJHVf9ebo_tQeh4VhJBkw0KSlQkDwDk3cdXU2UUddH1ZRd5loQNg24WVF3YVdVhUv8RDV1kYQln35MEW1MjAzJStyXRBkc5NzV0ZDY0c2E_1bJP3GRLZVPEfU19HcVq5A0D1HNRbOLLaR4EBQBFVW1mvRRPYcC1ExsVadVr3OWwT95sCNR86sFwOj9-Zji8ViccCQkaLm1CZbKRbWxhbK8tbHqG_NXmpl4crX2FzKp-LlwWSjKAy2ZMA9a4x31QHdxPTa6ly8U681Fl8-YfHJBSoJlF2YZqWWQZW2U8oBmRPCJjdNOF64emZvOtsI9vZO2Fp4kvqCPImCvXKmbZ-kiG5DtCDRJwiM4ISVzeTi5JQiq5OoqKrkhqJJkimBgIvfpCkXhk4YXzth7IZo_EW6qd2tcndoaGtAASm64ICBPitP3pkpWkd0verJ2lYdTt1cpPuFDxaSG0YWViYyZZV_ZCFNh8Kl4aSr25V4Jd2YyfIV_YB2Jb0l7tI4uDipiglLmB1lFGbaEAPKLPP6IVmUTbjpBDeMDdjplpU_1yFotybDcGHFBQvJYp62ebsWpGVpTStEkS9Xi6E4Wu2SQT8tJGGattFKFJmqLKdm91YXhs7eaDcMc8UBVxZzK8XIxAlpFA_KkQiSDuxNjYTzW1IKQevEpJUnaY8o083JbKurTnBBvam5UldTWT6wVi9Eg9FisDubqgQIR9TxYFn3C3NKAhtVqRdUZyYoFU2a0jeRokkUZhN7kgwLqR7SRjUzY4u9uQd4i_NfYC56h_E3OP8ZmofIym4rf4ZckWJupbpgZGZuZ0uYonyBdxh4icdInuEtHmJpKa0nr0g2r9hLRlakTAA=&g=w-w-w-w1HQHw-w2Xw-w3Xw-w2HQH
 
-  const generateCode = () => {
-    console.log("TODO: Generate QR code for " + inputUrl.value);
-    qrUrl.value = inputUrl.value
-  }
+const generateCode = () => {
+  console.log("TODO: Generate QR code for " + inputUrl.value);
+  qrUrl.value = inputUrl.value;
+};
+
+const onReady = (canvas: any) => {
+  const context = canvas.getContext("2d");
+  const image = new Image();
+
+  image.src = "/lexaloffle-pico8.png";
+  image.crossOrigin = "anonymous";
+  image.onload = () => {
+    const coverage = 0.15;
+    const width = Math.round(canvas.width * coverage);
+    const x = (canvas.width - width) / 2;
+
+    drawImage(context, image, x, x, width, width);
+  };
+};
+
+const drawImage = (context, image, x, y, width, height, radius = 4) => {
+  // context.shadowOffsetX = 0;
+  // context.shadowOffsetY = 2;
+  // context.shadowBlur = 4;
+  // context.shadowColor = '#00000040';
+  // context.lineWidth = 8;
+  // context.beginPath();
+  // context.moveTo(x + radius, y);
+  // context.arcTo(x + width, y, x + width, y + height, radius);
+  // context.arcTo(x + width, y + height, x, y + height, radius);
+  // context.arcTo(x, y + height, x, y, radius);
+  // context.arcTo(x, y, x + width, y, radius);
+  // context.closePath();
+  // context.strokeStyle = '#fff';
+  // context.stroke();
+  // context.clip();
+  // context.fillStyle = '#fff';
+  // context.fillRect(x, x, width, height);
+  context.drawImage(image, x, x, width, height);
+};
 </script>
