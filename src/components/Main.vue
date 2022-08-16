8afdf004e740928a790bbb929176ea51747c17eb
Paul Nicholas <liquidream@hotmail.co.uk>
2022-08-16 06:38:28 +0100

    Better styles/layout

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,17 @@
 <template>
-  <v-sheet class="pa-12">
+  <v-sheet class="pa-12 mx-auto">
     <v-sheet
       rounded="xl"
-      color="#065AB5"
+      elevation="12"
+      color="light-blue-lighten-3"
       class="mx-auto pa-12"
-      height="400"
-      width="100%"
+      width="800"
     >
+      <h1 class="text-h3">PICO-8 Web Tools</h1>
+      <span class="text-body-1">sdsdsdsfsdf</span>
+
+      <h2 class="text-h4 pt-5">QR-Code Generator</h2>
+      <span class="text-body-1">sdsdsdsfsdf</span>
       <v-card width="400">
         <v-card-item>
           <v-card-title>This is a title</v-card-title>
@@ -14,7 +19,7 @@
           <v-card-subtitle>This is a subtitle</v-card-subtitle>
         </v-card-item>
 
-        <v-card-text> This is content </v-card-text>
+        <v-card-text>This is content</v-card-text>
       </v-card>
     </v-sheet>
   </v-sheet>
