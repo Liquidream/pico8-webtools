f9d6714ab1e8632a2ba993f0923b55da79a4e20b
Paul Nicholas <liquidream@hotmail.co.uk>
2022-08-20 19:36:41 +0100

    Added P8 icon in header

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -7,16 +7,20 @@
       class="mx-auto pa-12"
       :width="mobile ? undefined : 800"
     >
-      <h1 class="text-h3">PICO-8 Web Tools</h1>
-      <span class="text-body-1"
+      <!-- <v-img src="lexaloffle-pico8.png" height="64px"></v-img> -->
+      <div class="header">
+        <img :src="pico8icon" />
+        <h1 class="text-h3">PICO-8 Web Tools</h1>
+      </div>
+      <div class="text-body-1 pt-5" style="clear: both;"
         >These web tools were designed for use with the
         <a href="https://www.pico-8-edu.com" target="_blank"
           >Education Edition of PICO-8.</a
         ><br />Such as generating QR codes or Itch.io web files for your
         creations.
-      </span>
+      </div>
       <qr-code-generator />
-      <itch-zip-builder />
+      <itch-zip-builder />      
     </v-sheet>
   </v-sheet>
 </template>
@@ -27,4 +31,17 @@ import QrCodeGenerator from "./QrCodeGenerator.vue";
 import ItchZipBuilder from "./ItchZipBuilder.vue";
 
 const { mobile } = useDisplay();
+const pico8icon = import.meta.env.BASE_URL + "lexaloffle-pico8.png";
 </script>
+
+<style scoped>
+.header img {
+  float: left;
+  width: 50px;
+}
+
+.header h1 {
+  position: relative;  
+  left: 10px;
+}
+</style>
--- src/components/QrCodeGenerator.vue
+++ src/components/QrCodeGenerator.vue
@@ -31,6 +31,7 @@ import VueQrcode from "@chenfengyuan/vue-qrcode";
 
 const inputUrl = ref("");
 const qrUrl = ref("");
+const pico8icon = import.meta.env.BASE_URL + "lexaloffle-pico8.png";
 
 // https://www.pico-8-edu.com/?c=cHJpbnQgImhlbGxvIHdvcmxkIg==&g=w-w-w-w1HQHw-w2Xw-w3Xw-w2HQH
 // https://www.pico-8-edu.com/?c=AHB4YQOnAslvcM4NR1zyCNEZ9yfnBssG3-u8wfmXv8GD5E8QPsHxdfMEzUYz9wjtS0RPED1BUjRPUgx02Ss8RLxUh0mXRO8QJHVf9ebo_tQeh4VhJBkw0KSlQkDwDk3cdXU2UUddH1ZRd5loQNg24WVF3YVdVhUv8RDV1kYQln35MEW1MjAzJStyXRBkc5NzV0ZDY0c2E_1bJP3GRLZVPEfU19HcVq5A0D1HNRbOLLaR4EBQBFVW1mvRRPYcC1ExsVadVr3OWwT95sCNR86sFwOj9-Zji8ViccCQkaLm1CZbKRbWxhbK8tbHqG_NXmpl4crX2FzKp-LlwWSjKAy2ZMA9a4x31QHdxPTa6ly8U681Fl8-YfHJBSoJlF2YZqWWQZW2U8oBmRPCJjdNOF64emZvOtsI9vZO2Fp4kvqCPImCvXKmbZ-kiG5DtCDRJwiM4ISVzeTi5JQiq5OoqKrkhqJJkimBgIvfpCkXhk4YXzth7IZo_EW6qd2tcndoaGtAASm64ICBPitP3pkpWkd0verJ2lYdTt1cpPuFDxaSG0YWViYyZZV_ZCFNh8Kl4aSr25V4Jd2YyfIV_YB2Jb0l7tI4uDipiglLmB1lFGbaEAPKLPP6IVmUTbjpBDeMDdjplpU_1yFotybDcGHFBQvJYp62ebsWpGVpTStEkS9Xi6E4Wu2SQT8tJGGattFKFJmqLKdm91YXhs7eaDcMc8UBVxZzK8XIxAlpFA_KkQiSDuxNjYTzW1IKQevEpJUnaY8o083JbKurTnBBvam5UldTWT6wVi9Eg9FisDubqgQIR9TxYFn3C3NKAhtVqRdUZyYoFU2a0jeRokkUZhN7kgwLqR7SRjUzY4u9uQd4i_NfYC56h_E3OP8ZmofIym4rf4ZckWJupbpgZGZuZ0uYonyBdxh4icdInuEtHmJpKa0nr0g2r9hLRlakTAA=&g=w-w-w-w1HQHw-w2Xw-w3Xw-w2HQH
@@ -44,7 +45,7 @@ const onReady = (canvas: any) => {
   const context = canvas.getContext("2d");
   const image = new Image();
   // https://vitejs.dev/guide/build.html#public-base-path
-  image.src = import.meta.env.BASE_URL + "lexaloffle-pico8.png";
+  image.src = pico8icon
   image.crossOrigin = "anonymous";
   image.onload = () => {
     const coverage = 0.15;
